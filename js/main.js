97875b0b4ff87a7c08ec472110cab129cfec524a
francescofalcone05 <ciccio_97@live.it>
2024-04-22 12:32:31 +0300

    corrected

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-/*-Scrivi un programma che stampi in console i numeri da 1 a 100, 
+/*
+-Scrivi un programma che stampi in console i numeri da 1 a 100, 
 -ma che per i multipli di 3 stampi “Fizz” al posto del numero
--e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.*/
+-e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+*/
 
 
 let numOne = 1
@@ -12,34 +14,43 @@ let fizzBuzz = 'FizzBuzz'
 
 
 for (numOne; numOne <= 100; numOne++) {
-    
 
-    
+    let contenuto = ""
+    let myClass = ''
+
     if ((numOne % mTre == 0) && (numOne % mCinque == 0)) {
-        // console.log(numOne = `FizzBuzz`)
-        //console.log(`FizzBuzz`)
-        //    numOne = `${fizzBuzz}`
+        //numOne = `${fizzBuzz}`
+        //numOne = fizzBuzz
+        contenuto = fizzBuzz
+        myClass = `my-class-1`
         console.log(`${numOne} è multiplo di entrambi`)
-        
+
     } else if (numOne % mTre == 0) {
         // numOne = fizz
-        //  numOne = 'fizz'
+        // numOne = 'fizz'
+        myClass = `my-class-2`
+        contenuto = fizz
         console.log(`${numOne} è multiplo di ${mTre}`)
-        
+
     } else if (numOne % mCinque == 0) {
         // numOne = buzz
-        //  numOne = 'Buzz'
-        console.log(`${numOne} è multiplo di ${mCinque}`)   
+        // numOne = 'Buzz'
+        myClass = `my-class-3`
+        contenuto = buzz
+        console.log(`${numOne} è multiplo di ${mCinque}`)
     } else {
+        myClass = `my-class-4`
+        contenuto = numOne
         console.log(numOne)
     }
+    
 
 
     const myUl = document.querySelector('ul');
     const newLi = document.createElement('li');
-    newLi.append(numOne);
-    newLi.classList.add('my-class-1');
+    newLi.append(contenuto);
+    newLi.classList.add(myClass);
     myUl.append(newLi);
-    
+
 }
 
